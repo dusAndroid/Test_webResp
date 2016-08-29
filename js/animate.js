d0d86dbe762714080114f33925a0ada46ea673a9
dusAndroid <duscodd@163.com>
2016-08-29 17:12:15 +0800

    提交旋转轮播图代码

--- js/animate.js
+++ js/animate.js
@@ -1,27 +1,19 @@
-/**
- * Created by andy on 2015/11/23.
- */
 // 多个属性运动框架  添加回调函数
 function animate(obj,json,fn) {  // 给谁    json
     clearInterval(obj.timer);
     obj.timer = setInterval(function() {
-        var flag = true;  // 用来判断是否停止定时器   一定写到遍历的外面
-        for(var attr in json){   // attr  属性     json[attr]  值
-            //开始遍历 json
-            // 计算步长    用 target 位置 减去当前的位置  除以 10
-            // console.log(attr);
+        var flag = true;
+        for(var attr in json){
+
             var current = 0;
             if(attr == "opacity")
             {
                 current = Math.round(parseInt(getStyle(obj,attr)*100)) || 0;
-                console.log(current);
             }
             else
             {
                 current = parseInt(getStyle(obj,attr)); // 数值
             }
-            // console.log(current);
-            // 目标位置就是  属性值
             var step = ( json[attr] - current) / 10;  // 步长  用目标位置 - 现在的位置 / 10
             step = step > 0 ? Math.ceil(step) : Math.floor(step);
             //判断透明度
@@ -33,7 +25,7 @@ function animate(obj,json,fn) {  // 给谁    json
                     obj.style.opacity = (current + step) /100;
                 }
                 else
-                {  // obj.style.filter = alpha(opacity = 30)
+                {
                     obj.style.filter = "alpha(opacity = "+(current + step)* 10+")";
 
                 }
@@ -47,7 +39,7 @@ function animate(obj,json,fn) {  // 给谁    json
                 obj.style[attr] = current  + step + "px" ;
             }
 
-            if(current != json[attr])  // 只要其中一个不满足条件 就不应该停止定时器  这句一定遍历里面
+            if(current != json[attr])
             {
                 flag =  false;
             }
@@ -55,15 +47,14 @@ function animate(obj,json,fn) {  // 给谁    json
         if(flag)  // 用于判断定时器的条件
         {
             clearInterval(obj.timer);
-            //alert("ok了");
-            if(fn)   // 很简单   当定时器停止了。 动画就结束了  如果有回调，就应该执行回调
+            if(fn)
             {
-                fn(); // 函数名 +  （）  调用函数  执行函数
+                fn();
             }
         }
     },10)
 }
-function getStyle(obj,attr) {  //  谁的      那个属性
+function getStyle(obj,attr) {
     if(obj.currentStyle)  // ie 等
     {
         return obj.currentStyle[attr];  // 返回传递过来的某个属性
